3eed6ee412ce1d83b16a3e1e41ea61229305a126
jHoon0223 <aleejaehun@naver.com>
2023-03-27 07:38:16 +0000

    230327 : long long int 합 연산

---
+++ 201sabde/BAEKJOON_re/입출력과 사칙연산/11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    long long int a,b,c;
+    cin >> a >> b >> c;
+
+    long long int sum = (long long int)a + (long long int)b + (long long int)c;
+    printf("%lld\n", sum);
+
+    return 0;
+}
