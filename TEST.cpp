eb46f28efbf699175eb36151d7d1ad06287f8cda
jHoon0223 <aleejaehun@naver.com>
2023-05-18 14:15:14 +0000

    230518 : map 이용해서 인덱스 횟수 계산하기

--- TEST.cpp
+++ TEST.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    printf("         ,r'\"7\n");
-    printf("r`-_   ,'  ,/\n");
-    printf(" \\. \". L_r'\n");
-    printf("   `~\\/\n");
-    printf("      |\n");
-    printf("      |\n");
+    double d = 1.1;
+    printf("%d\n", (int)d);
+    cout << (int)d << endl;
 
     return 0;
 }
