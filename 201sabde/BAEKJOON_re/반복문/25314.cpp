1db179e300c5e2365323ce73726b301c3f774617
jHoon0223 <aleejaehun@naver.com>
2023-07-12 13:50:24 +0000

    230712 : 반복문 - 코딩은 체육과목 입니다

---
+++ 201sabde/BAEKJOON_re/반복문/25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = n/4;
+    while(a-->0) printf("long ");
+    printf("int\n");
+
+    return 0;
+}
