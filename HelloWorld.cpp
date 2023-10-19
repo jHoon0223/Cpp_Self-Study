b6fff38a08ce7393f8cb1f184069a09dfa618ae5
jHoon0223 <aleejaehun@naver.com>
2023-10-19 14:02:11 +0900

    231015 : C++ 개발환경 세팅 / json 파일 수정

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int a;
+    cin >> a;
+
+    vector<int> v;
+
+    for (int i=0; i<a; i++) {
+        int n;
+        cin >> n;
+        v.push_back(n);
+    }
+    sort(v.begin(), v.end());
+
+    for (int i=0; i<v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
