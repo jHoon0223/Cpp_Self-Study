810383e2aa94d183ac95835d86c4c42dc8d28e9d
jHoon0223 <aleejaehun@naver.com>
2023-11-01 14:37:12 +0900

    231101 : 카드2 - 큐 활용

---
+++ BOJ/201sabde/스택, 큐, 덱/2164.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    queue<int> q;
+
+    for (int i=1; i<=N; i++) {
+        q.push(i);
+    }
+
+    while(1) {
+        if (q.size() == 1) break;
+        q.pop();
+
+        if (q.size() == 1) break;
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front() << '\n';
+
+    return 0;
+}
