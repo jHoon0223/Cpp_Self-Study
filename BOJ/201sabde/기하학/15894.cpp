ad73c103ba91683f7c5fbb47322aec3c1876f09c
jHoon0223 <aleejaehun@naver.com>
2023-12-05 14:12:08 +0900

    231204 : 수학은 체육과목입니다.

---
+++ BOJ/201sabde/기하학/15894.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    long long int n;
+    cin >> n;
+
+    cout << 4*n << "\n";
+
+    return 0;
+}
