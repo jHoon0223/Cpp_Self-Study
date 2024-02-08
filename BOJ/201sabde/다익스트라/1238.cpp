5645a1c714fd10dc9d80addf329ce69a272ac04c
jHoon0223 <aleejaehun@naver.com>
2024-02-08 16:56:21 +0900

    240208 : #1238 - 왕복 경로에 대한 다익스트라 수행

--- BOJ/201sabde/다익스트라/1238.cpp
+++ BOJ/201sabde/다익스트라/1238.cpp
@@ -12,31 +12,7 @@ vector<pair<int,int>> Graph[MAX];
 int costX[MAX];
 int cost[MAX];
 
-void dijkstraX(int start) {
-    costX[start] = 0;
-    priority_queue<pair<int,int>> pq;
-    pq.push(make_pair(start,0));
-
-    while (!pq.empty()) {
-        int current = pq.top().first;
-        int dist = -pq.top().second;
-        pq.pop();
-
-        if (costX[current] < dist) continue;
-
-        for (int i=0; i<Graph[current].size(); i++) {
-            int newCurr = Graph[current][i].first;
-            int newDist = dist + Graph[current][i].second;
-
-            if (newDist < costX[newCurr]) {
-                costX[newCurr] = newDist;
-                pq.push(make_pair(newCurr, -newDist));
-            }
-        }
-    }
-}
-
-void dijkstra(int start) {
+void dijkstra(int start, int cost[]) {
     cost[start] = 0;
     priority_queue<pair<int,int>> pq;
     pq.push(make_pair(start,0));
@@ -76,14 +52,14 @@ int main() {
     }
 
     for (int i=1; i<=V; i++) costX[i] = INF;
-    dijkstraX(X);
+    dijkstra(X, costX);
 
     for (int i=1; i<=V; i++) {
         for (int i=1; i<=V; i++) cost[i] = INF;
 
         int target = i;
 
-        dijkstra(target);
+        dijkstra(target, cost);
         costX[target] += cost[X];
     }
 
