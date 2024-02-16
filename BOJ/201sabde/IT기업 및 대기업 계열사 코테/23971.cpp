8e93f01f094a61e86b5ab38ab1a187b7ca572123
jHoon0223 <aleejaehun@naver.com>
2024-02-16 14:23:36 +0900

    240216 : #23971

---
+++ BOJ/201sabde/IT기업 및 대기업 계열사 코테/23971.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double h, w, n, m;
+
+int main() {
+    cin >> h >> w >> n >> m;
+
+    cout << (int)ceil(h / (n+1)) * (int)ceil(w / (m+1)) << '\n';
+}
