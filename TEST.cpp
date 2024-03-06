a3ac14a05d8bc78d0560ebd24995c20e3f09d8dd
jHoon0223 <aleejaehun@naver.com>
2024-03-06 15:48:13 +0900

    240306 : #1978 - 소수 판정

--- TEST.cpp
+++ TEST.cpp
@@ -1,55 +1,21 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main() {
-    cin.tie();
-    cout.tie();
-    ios::sync_with_stdio(false);
+	int N, result = 0;
+	int temp, cnt = 0;
 
-    int T;
-    cin >> T;
-    while (T--) {
-        int H, W, N;
-        cin >> H >> W >> N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
 
-        string total;
-        int xx = N/H;
-        int yy = N%H;
-
-        if (yy==0) {
-            xx = N/H;
-            yy = H;
-
-            if (xx < 10) {
-                string YY = to_string(yy);
-                string XX = "0" + to_string(xx);
-
-                total = YY + XX;
-                cout << total << '\n';
-            }
-            else {
-                total = to_string(yy) + to_string(xx);
-                cout << total << '\n';
-            }
-        }
-        else {
-            xx++;
-
-            if (xx < 10) {
-                string YY = to_string(yy);
-                string XX = "0" + to_string(xx);
-
-                total = YY + XX;
-                cout << total << '\n';
-            }
-            else {
-                total = to_string(yy) + to_string(xx);
-                cout << total << '\n';
-            }
-        }
-    }
-
-    return 0;
+		for (int i = 1; i <= temp; i++) {
+			if (temp%i == 0)
+				cnt++;
+		}
+		if (cnt == 2)	//temp가 소수
+			result++;
+		cnt = 0;
+	}
+	cout << result << '\n';
 }
