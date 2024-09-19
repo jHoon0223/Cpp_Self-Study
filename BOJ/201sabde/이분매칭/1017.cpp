9e34c785dfea32d67b05bf408b35729749bdb806
jHoon0223 <aleejaehun@naver.com>
2024-09-19 18:28:40 +0900

    240919 : #1017 - 이분매칭 / 에라토스테네스의 체

--- BOJ/201sabde/이분매칭/1017.cpp
+++ BOJ/201sabde/이분매칭/1017.cpp
@@ -1,105 +1,71 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <set>
+#include <algorithm>
 
-#define MAX 25
+#define MAX 2000
 
 using namespace std;
 
-int N, cnt=0;
+int N;
 
-int matched[MAX];
-bool visited[MAX];
-
-vector<int> input;
-vector<int> mid_input;
-vector<int> v[MAX];
+bool prime[MAX];
+int input[50];
+int matched[50];
+bool visited[50];
 
 set<int> total;
 
-bool isF(int a, int b) {
-    int target = a+b;
-    for (int i=2; i<target; i++)
-        if (target % i == 0) return false;
-
-    return true;
-}
-
 bool DFS(int curr) {
-    for (int next : v[curr]) {
-        if (visited[next]) continue;
+    visited[curr] = true;
 
-        visited[next] = true;
+    for (int next=1; next<N; next++) {
+        if (next==curr || !prime[input[curr] + input[next]]) continue;
 
-        if (!matched[next] || DFS(matched[next])) {
+        if (!matched[next] || (!visited[matched[next]] && DFS(matched[next]))) {
             matched[next] = curr;
             return true;
         }
     }
-
     return false;
 }
 
 int main() {
     cin.tie(0); ios::sync_with_stdio(false);
 
+    fill(prime, prime+MAX, true);
+    for (int i=2; i*i<MAX; i++) {
+        if (!prime[i]) continue;
+        for (int j=i*i; j<MAX; j+=i) prime[j] = false;
+    }
+    //  미리 소수 판별
+
     cin >> N;
-    for (int i=0; i<N; i++) {
-        int a;
-        cin >> a;
+    for (int i=0; i<N; i++) cin >> input[i];
+    //  input Fin.
 
-        input.push_back(a);
-    }
-    sort(input.begin(), input.end());
+    for (int target=1; target<N; target++) {
+        if (!prime[input[0] + input[target]]) continue;
+        fill(matched, matched+N, 0);
+        
+        bool flag = true;
+        for (int _t=1; _t<N; _t++) {
+            if (target == _t) continue;
 
-    int target = input[0];
-    for (int tmp : input) {
-        if (target == tmp) continue;
+            fill(visited, visited+N, false);
+            visited[target] = true;
 
-        if (isF(target, tmp)) mid_input.push_back(tmp);
-        else continue;
-    }
-
-    for (int a : mid_input) {
-        cnt = 0;
-        fill(matched, matched+MAX, 0);
-
-        for (int _target : input) {
-            if (_target == target) continue;
-            else if (_target == a) continue;
-            else {
-                for (int __target : input) {
-                    if (__target == target) continue;
-                    else if (__target == a) continue;
-                    else {
-                        if (isF(_target, __target))
-                            v[_target].push_back(__target);
-                    }
-                }
-            }
+            if (DFS(_t) == false) flag = false;
         }
 
-        for (int t=0; t<N; t++) {
-            if (input[t] == target) continue;
-            else if (input[t] == a) continue;
-
-            fill(visited, visited+MAX, false);
-            matched[target] = a;
-            visited[a] = true;
-
-            if (DFS(input[t])) cnt++;
-        }
-
-        if (cnt+1 == N/2) total.insert(matched[target]);
-        else continue;
+        if (flag) total.insert(input[target]);
     }
 
     if (total.empty()) cout << -1 << '\n';
     else {
-        for (int a : total) cout << a << ' ';
+        for (int ans : total) cout << ans << ' ';
         cout << '\n';
     }
+    //  Fin.
 
     return 0;
 }
