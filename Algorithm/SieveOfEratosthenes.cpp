d0daf23654d3938399793c458886681b9ed131ec
jHoon0223 <aleejaehun@naver.com>
2024-09-19 17:51:12 +0900

    240919 : 에라토스테네스의 체 - 간단한 소수판별 알고리즘

--- Algorithm/SieveOfEratosthenes.cpp
+++ Algorithm/SieveOfEratosthenes.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
-const int MAX = 100000;
+void sieveOfEratosthenes(int n) {
+    // n까지의 숫자가 소수인지 여부를 저장하는 벡터
+    vector<bool> isPrime(n + 1, true);
+
+    // 0과 1은 소수가 아니므로 false로 설정
+    isPrime[0] = isPrime[1] = false;
+
+    // 2부터 시작하여 소수 여부를 판별
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            // i의 배수들은 소수가 아니므로 false로 설정
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    // 소수 출력
+    cout << "Prime numbers up to " << n << ": ";
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
 
 int main() {
     int n;
+    cout << "Enter the upper limit to find primes: ";
     cin >> n;
 
-    vector<int> primeVector;
-    bool isPrime[MAX];
-
-    for (int i = 0; i < n; i++)
-        isPrime[i] = true;
-
-    for (int i = 2; i <= n; i++) {
-        if (!isPrime[i]) continue;
-        else primeVector.push_back(i);
-
-        for (int j = i*2; j <= n; j += i)
-            isPrime[j] = false;
-    }
-
-    for (int i = 0; i < primeVector.size(); i++)
-        cout << primeVector[i] << " ";
+    sieveOfEratosthenes(n);
 
     return 0;
 }
