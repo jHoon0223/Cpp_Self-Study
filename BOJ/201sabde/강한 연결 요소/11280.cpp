c6d51539937374f0c85273daa58693760c91e6a2
jHoon0223 <aleejaehun@naver.com>
2024-09-27 11:57:58 +0900

    240925 : #11280 - SCC를 활용한 2-SAT 문제 해결하기

--- BOJ/201sabde/강한 연결 요소/11280.cpp
+++ BOJ/201sabde/강한 연결 요소/11280.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int N,M;
 int visited[MAX], node_scc[MAX], visited_num, scc_num;
 vector<vector<int>> SCC;
-vector<int> v[MAX];
+vector<int> graph[MAX];
 stack<int> st;
 
 inline int nX(int a) { return a ^ 1; }
@@ -18,30 +18,32 @@ inline int tX(int a) { return a * 2 - 2; }
 inline int fX(int a) { return a * 2 - 1; }
 
 int DFS(int curr) {
-    visited[curr] = visited_num++;
+    visited[curr] = visited_num;
+    visited_num++;
     st.push(curr);
     int val = visited[curr];
 
-    for (int next : v[curr]) {
+    for (int next : graph[curr]) {
         if (visited[next] == -1) val = min(val, DFS(next));
         else if (node_scc[next] == -1) val = min(val, visited[next]);
     }
 
     if (val == visited[curr]) {
-        vector<int> ret;
+        vector<int> tmp;
         
-        while(1) {
+        while (1) {
             int target = st.top();
             st.pop();
             node_scc[target] = scc_num;
-            ret.push_back(target);
+            tmp.push_back(target);
 
             if (target == curr) break;
         }
-
-        SCC.push_back(ret);
+        SCC.push_back(tmp);
         scc_num++;
     }
+
+    return val;
 }
 
 int main() {
@@ -56,8 +58,11 @@ int main() {
         int u,v;
         cin >> u >> v;
 
-        u > 0 ? tX(u) : fX(-u);
-        v > 0 ? tX(v) : fX(-v);
+        u = u > 0 ? tX(u) : fX(-u);
+        v = v > 0 ? tX(v) : fX(-v);
+
+        graph[nX(u)].push_back(v);
+        graph[nX(v)].push_back(u); 
     }
 
     for (int i=0; i<=N*2-1; i++) {
@@ -71,7 +76,6 @@ int main() {
             break;
         }
     }
-
     flag ? cout << "1\n" : cout << "0\n";
 
     return 0;
