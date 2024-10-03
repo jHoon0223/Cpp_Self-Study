6812a3df19600a1ac01e9136be66c9257fd1f9f2
jHoon0223 <aleejaehun@naver.com>
2024-10-03 12:46:08 +0900

    241003 : #1956 - 플로이드 워셜을 이용한 사이클 판별

---
+++ BOJ/201sabde/플로이드-워셜/1956.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <set>
+
+#define MAX 401
+#define INF 99999999
+
+using namespace std;
+
+int V,E;
+int arr[MAX][MAX];
+
+void floydWarshall() {
+    for (int k=0; k<V; k++) {
+        for (int i=0; i<V; i++) {
+            for (int j=0; j<V; j++) {
+                if (arr[i][k] != INF && arr[k][j] != INF)
+                    arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+            }
+        }
+    }
+}
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+
+    cin >> V >> E;
+
+    for (int i=0; i<V; i++) {
+        for (int j=0; j<V; j++) {
+            arr[i][j] = INF;
+        }
+    }
+
+    for (int i=0; i<E; i++) {
+        int a,b,c;
+        cin >> a >> b >> c;
+        a--; b--;
+        arr[a][b] = c;
+    }
+
+    floydWarshall();
+
+    set<int> total;
+    for (int i=0; i<V; i++) {
+        for (int j=0; j<V; j++) {
+            if (i==j && arr[i][j] != INF) {
+                total.insert(arr[i][j]);
+            }
+        }
+    }
+
+    if (total.empty()) cout << -1 << '\n';
+    else cout << *total.begin() << '\n';
+
+    return 0;
+}
