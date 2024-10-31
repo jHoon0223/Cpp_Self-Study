d5899855389094bdfb23daab6d3ee23f2f6ea3b3
jHoon0223 <aleejaehun@naver.com>
2024-10-31 19:14:06 +0900

    241031 : #10407 - 정수론

---
+++ BOJ/SCCC/3주차/10407.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+
+    long long H;
+    cin >> H;
+
+    if (H==1) cout << 2 << '\n';
+    else cout << 1 << '\n';
+
+    return 0;
+}
