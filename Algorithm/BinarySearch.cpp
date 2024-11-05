9bb1e3a577d534739df0e4fb5d9fbff26f95d7a6
jHoon0223 <aleejaehun@naver.com>
2024-11-05 17:01:30 +0900

    241105 : lower_bound 활용한 이진탐색

--- Algorithm/BinarySearch.cpp
+++ Algorithm/BinarySearch.cpp
@@ -5,17 +5,28 @@ using namespace std;
 
 vector<int> v;
 
-bool BinarySearch(int target, int left, int right) {
-    int mid;
+// bool binarySearch(int target) {
+//     int left = 0;
+//     int right = v.size() - 1;
 
-    while (left <= right) {
-        mid = (left+right) / 2;
+//     while (left <= right) {
+//         int mid = left + (right - left) / 2;  // 중간 인덱스 계산
+        
+//         if (v[mid] == target) return true;  // target을 찾음
+//         else if (v[mid] < target) left = mid + 1;  // target이 오른쪽에 있음
+//         else right = mid - 1;  // target이 왼쪽에 있음
+//     }
+    
+//     return false;  // target을 찾지 못함
+// }
 
-        if (target == v[mid]) return true;
-        else if (target < v[mid]) right = mid-1;
-        else if (target > v[mid]) left = mid+1;
-    }
-    return false;
+bool binarySearch(int target) {
+    // lower_bound를 사용하여 target 이상의 첫 번째 위치를 찾고, 인덱스로 변환
+    int index = lower_bound(v.begin(), v.end(), target) - v.begin();
+    
+    // 배열의 범위를 벗어나지 않고, 해당 위치의 값이 target과 같다면 target이 존재
+    if (index < v.size() && v[index] == target) return true;
+    else return false;
 }
 
 int main(void) {
@@ -31,9 +42,9 @@ int main(void) {
     cout << "target >> ";
     cin >> target;
 
-    flag = BinarySearch(target, 0, v.size());
-    if (flag) cout << "YES";
-    else cout << "NO";
+    flag = binarySearch(target);
+    if (flag) cout << "YES\n";
+    else cout << "NO\n";
 
     return 0;
 }
